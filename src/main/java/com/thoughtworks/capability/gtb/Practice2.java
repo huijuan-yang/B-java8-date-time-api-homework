7f0ca2d7211864f7be33300be8c2643c3509c37c
Yvonne Yang _ ACI <yvonneyang_aci@Huijuans-MacBook-Pro.local>
2020-11-17 11:01:08 +0800

    finished practice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 /**
@@ -11,6 +12,12 @@ import java.time.LocalDate;
 public class Practice2 {
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    if (date.getDayOfWeek() == DayOfWeek.FRIDAY) {
+      return date.plusDays(3);
+    } else if (date.getDayOfWeek() == DayOfWeek.SATURDAY) {
+      return date.plusDays(2);
+    } else {
+      return date.plusDays(1);
+    }
   }
 }
